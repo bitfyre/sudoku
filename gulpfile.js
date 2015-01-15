8a73ab8b80ed31c609269e52c50a2d3f9822551e
Alex Lemanski <alex.lemanski@bitfyre.net>
2015-01-15 14:07:41 -0800

    Update html task to use jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var browserSync = require('browser-sync');
 var gulp = require('gulp');
+var jade = require('gulp-jade');
 var path = require('path');
+var rename = require('gulp-rename');
 
 gulp.task('html', function(){
-  gulp.src(path.join(__dirname, 'src/**/*.html'))
+  gulp.src(path.join(__dirname, 'src/**/*.html.jade'))
+    .pipe(rename({
+      // Strips .jade extension leaving .html
+      extname: ''
+    }))
+    .pipe(jade({
+      pretty: true
+    }))
     .pipe(gulp.dest(path.join(__dirname, 'build')));
 });
